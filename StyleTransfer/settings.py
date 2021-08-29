09ecfa0ff70fa4b991703231c3a2f6a5e9b05197
Bui Duc Anh <ducanh6901@gmail.com>
2021-08-29 09:51:25 +0900

    add cloudinary

--- StyleTransfer/settings.py
+++ StyleTransfer/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
+    'cloudinary_storage',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -128,5 +130,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'myapp/media/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME':'hdqwzvuhp',
+    'API_KEY' : '661979467525411',
+    'API_SECRET': '0ycFKZTEvRRZ1flWeyecOg0dnLM',
+}
+
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 django_heroku.settings(locals())
